174c9a31b2ac1709a4a0c208c3ac05605fb3b790
JadeEtoile <robotx@osakaweb.jp>
2019-09-13 21:16:27 +0900

    Reconstruct Algorithms of PI Controller

--- include/velocity_controller/velocity_controller.hpp
+++ include/velocity_controller/velocity_controller.hpp
@@ -33,12 +33,23 @@ private:
   
   
   /*Member Variables*/
-  double target_velocity_[3]; /* [u, v, r] m/s*/
-  double current_velocity_[3]; /* [u, v, r] m/s*/
+  double target_velocity_y_; /* [u, v, r] m/s*/
+  double target_velocity_yaw_; /* [u, v, r] m/s*/
+  double current_velocity_y_; /* [u, v, r] m/s*/
+  double current_velocity_yaw_; /* [u, v, r] m/s*/
+  double velocity_difference_y_old;
+  double velocity_difference_yaw_old;
+  double thrust_left_old_;
+  double thrust_right_old_;
   double base_thrust_;
-  double param_max_velocity_[3]; /* [u, v, r] m/s : Max Speeds of the ship*/
-  double param_max_thrust_deviation_[3]; /* [u, v, r] % : Max gradient of Speeds*/
-  double param_p_gain_[3]; /* [u, v, r] m/s : Parameter of control gain (Proportional) */
+  double rotation_thrust_;
+  double param_max_velocity_y_; /* [u, v, r] m/s : Max Speeds of the ship*/
+  double param_max_velocity_yaw_; /* [u, v, r] m/s : Max Speeds of the ship*/
+  double param_max_thrust_deviation_; /* [u, v, r] % : Max gradient of Speeds*/
+  double param_p_gain_y_; /* [u, v, r] m/s : Parameter of control gain (Proportional) */
+  double param_p_gain_yaw_;
+  double param_i_gain_y;
+  double param_i_gain_yaw;
   usv_control_msgs::AzimuthThrusterCatamaranDriveStamped thrust_cmd_;
   
   /*Parameters*/
@@ -52,8 +63,7 @@ private:
   void subcb_velcurr(const geometry_msgs::TwistStamped::ConstPtr msg);
   void reconf_cbfunc(velocity_controller::ControlParamConfig &config, uint32_t level);
   void update_thrust();
-  void publish_thrust();
-  
+  void publish_thrust();  
 };
 
 #endif  /*VELOCITY_CONTROLLER_H_INCLUDED*/
--- src/velocity_controller.cpp
+++ src/velocity_controller.cpp
@@ -1,3 +1,4 @@
+ls
 // Headers in this package
 #include <velocity_controller/velocity_controller.hpp>
 #include <boost/bind.hpp>
@@ -16,7 +17,12 @@ VelocityController::VelocityController(ros::NodeHandle nh, ros::NodeHandle pnh):
 	  target_velocity_[cnt] = 0.0;
 	  current_velocity_[cnt] = 0.0;
 	}
+  velocity_difference_y_old = 0.0;
+  velocity_difference_yaw_old = 0.0;
+  thrust_left_old_ = 0.0;
+  thrust_right_old_ = 0.0;
   base_thrust_ = 0.0;
+  rotation_thrust_ = 0.0;
   
   /* Create ROS Param */
   pnh_.param<std::string>("target_velocity", sub_topicname_target_velocity_, "/target_velocity");
@@ -42,6 +48,7 @@ VelocityController::~VelocityController()
   
 }
 
+
 void VelocityController::run()
 {
   ros::Rate rate(100);
@@ -60,105 +67,95 @@ void VelocityController::run()
 void VelocityController::subcb_veltarg(const geometry_msgs::TwistStamped::ConstPtr msg)
 {
   mtx_.lock();
-  target_velocity_[0] = msg->twist.linear.x;
-  target_velocity_[1] = msg->twist.linear.y;
-  target_velocity_[2] = msg->twist.angular.z;
+  target_velocity_y_ = msg->twist.linear.y;
+  target_velocity_yaw_ = msg->twist.angular.z;
   mtx_.unlock();
-  
 }
+
 
 void VelocityController::subcb_velcurr(const geometry_msgs::TwistStamped::ConstPtr msg)
 {
   mtx_.lock();
-  current_velocity_[0] = msg->twist.linear.x;
-  current_velocity_[1] = msg->twist.linear.y;
-  current_velocity_[2] = msg->twist.angular.z;
+  current_velocity_y_ = msg->twist.linear.y;
+  current_velocity_yaw_ = msg->twist.angular.z;
   mtx_.unlock();
 }
+
 
 void VelocityController::reconf_cbfunc(velocity_controller::ControlParamConfig &config, uint32_t level)
 {
   mtx_.lock();
-  param_max_velocity_[0] = config.VctrlMax_X;
-  param_max_velocity_[1] = config.VctrlMax_Y;
-  param_max_velocity_[2] = config.VctrlMax_Yaw;
-
-  param_max_thrust_deviation_[0] = config.TdiffMax_X;
-  param_max_thrust_deviation_[1] = config.TdiffMax_Y;
-  param_max_thrust_deviation_[2] = config.TdiffMax_Yaw;
-
-  param_p_gain_[0] = config.PGain_X;
-  param_p_gain_[1] = config.PGain_Y;
-  param_p_gain_[2] = config.PGain_Yaw;
+  param_max_velocity_y_ = config.VctrlMax_Y;
+  param_max_velocity_yaw_ = config.VctrlMax_Yaw;
+  param_max_thrust_deviation_ = config.TdiffMax;
+  param_p_gain_y_ = config.PGain_X;
+  param_p_gain_yaw_ = config.PGain_Yaw;
+  param_i_gain_y_ = config.IGain_X;
+  param_i_gain_yaw_ = config.IGain_Yaw;
   mtx_.unlock();
 }
 
+
 void VelocityController::update_thrust()
 {
-  int cnt = 0;
-  double Vdiff[3] = {0.0, 0.0, 0.0}; /*u, v, r*/
-  double Vdiff_idx[3] = {0.0, 0.0, 0.0};
+  /*Parameters*/
+  double max_velocity_y = 0.0;
+  double max_velocity_yaw = 0.0;
+  double max_thrust_deviation = 0.0;
+  double p_gain_y = 0.0;
+  double p_gain_yaw = 0.0;
+  double i_gain_y = 0.0;
+  double i_gain_yaw = 0.0;
+  
+  /*Variables*/
+  double velocity_difference_y = 0.0;
+  double velocity_difference_yaw = 0.0;
+  double max_thrust = 0.0;
   double T_Left = 0.0;
   double T_Right = 0.0;
   double T_Left_abs = 0.0;
   double T_Right_abs = 0.0;
-  double Vctrl_max[3];
-  double Tdiff_max[3];
-  double Pgain[3];
-  
-  mtx_.lock();
-  for(cnt=0; cnt<3; cnt++)
-  {
-    Vctrl_max[cnt] = param_max_velocity_[cnt];
-    Tdiff_max[cnt] = param_max_thrust_deviation_[cnt];
-    Pgain[cnt] = param_p_gain_[cnt];
-  }
-  
-  /* -1- Getting Current Velocity Status */
-  for(cnt=0; cnt<3; cnt++)
-  {
-	  Vdiff[cnt] = target_velocity_[cnt] - current_velocity_[cnt];
-  }
-  ROS_INFO("Vel-DEV  X:%.3lf  Y:%.3lf  YAW:%.3lf", Vdiff[0], Vdiff[1], Vdiff[2]);
-  
-  /* -2- Normalize Vdiff */
-  for(cnt=0; cnt<3; cnt++)
-  {
-    Vdiff_idx[cnt] = Vdiff[cnt] / Vctrl_max[cnt];
-    ROS_INFO("Vel-DEVIDX  X:%.3lf", Vdiff_idx[cnt]);	
-    if( Vdiff_idx[cnt] > Tdiff_max[cnt] )
-    {
-      Vdiff_idx[cnt] = Tdiff_max[cnt];
-    }
-    else if( Vdiff_idx[cnt] < -Tdiff_max[cnt] )
-    {
-      Vdiff_idx[cnt] = -Tdiff_max[cnt];
-    }
-  }
-  ROS_INFO("Vel-DEV(STD,CAP)  X:%.6lf  Y:%.6lf  YAW:%.6lf", Vdiff_idx[0], Vdiff_idx[1], Vdiff_idx[2]);
 
-  /* -3- Calculate Base Thrust */
-  base_thrust_ =  base_thrust_ + Pgain[0] * Vdiff_idx[0];
-  if(base_thrust_ > 1.0)
-  {
-	  base_thrust_ = 1.0;
-  }
-  else if(base_thrust_ < -1.0)
-  {
-	  base_thrust_ = -1.0;
-  }
-  ROS_INFO("Base Thrust: %.3lf", base_thrust_);
-	
-  /* -4- Calculate Differential Thrust */
-  T_Left = base_thrust_ + Pgain[2] * (Vdiff_idx[2] / 2);
-  T_Right = base_thrust_ - Pgain[2] * (Vdiff_idx[2] / 2);
+
+  
+  /* -0- Getting Parameters */
+  mtx_.lock();
+  max_velocity_y = param_max_velocity_y_;
+  max_velocity_yaw = param_max_velocity_yaw_;
+  max_thrust_deviation = param_max_thrust_deviation;
+  p_gain_y = param_p_gain_y_;
+  p_gain_yaw = param_p_gain_yaw_;
+  i_gain_y = param_i_gain_y_;
+  i_gain_yaw = param_i_gain_yaw_;
+
+  
+  /* -1- Calculate difference between target and current velocity */
+  velocity_difference_y = target_velocity_y_ - current_velocity_y_;
+  velocity_difference_yaw = target_velocity_yaw_ - current_velocity_yaw_;
+  mtx_.unlock();
+  ROS_INFO("Vel-Diff  Y:%.3lf  YAW:%.3lf", velocity_difference_y, velocity_difference_yaw);
+
+  
+  /* -2- Calculate Base Thrust and Rotation Thrust */
+  base_thrust_ = p_gain_y * (velocity_difference_y - velocity_difference_y_old) + i_gain_y * velocity_difference_y;
+  rotation_thrust_ = p_gain_yaw * (velocity_difference_yaw - velocity_difference_yaw_old) + i_gain_yaw * velocity_difference_yaw;
+  ROS_INFO("Fwd:%.3lf, Rot:%.3lf", base_thrust_, rotation_thrust_);
+
+  
+  /* -3- Calculate Left and Right Thrust */
+  T_Left = base_thrust_ - rotation_thrust;
+  T_Right = base_thrust_ + rotation_thrust;
   ROS_INFO("Thrust(RAW): [LEFT]%.3lf, [RIGHT]%.3lf", T_Left, T_Right);
   
-  
-  /* -5- Adjust to Maximum Thrust */
+  /* -4- Normalize Thrust */
+  max_thrust =
+	p_gain_y * max_velocity_y + i_gain_yaw * max_velocity_y
+	+p_gain_yaw * max_velocity_yaw + i_gain_yaw * max_velocity_yaw;
+  T_Left /= max_thrust;
+  T_Right /= max_thrust;
+
   T_Left_abs = std::abs(T_Left);
-  T_Right_abs = std::abs(T_Right);
-  
+  T_Right_abs = std::abs(T_Right);  
   if( T_Left_abs >1.0 || T_Right_abs > 1.0 )
   {
     if( T_Left_abs > T_Right_abs )
@@ -172,11 +169,44 @@ void VelocityController::update_thrust()
       T_Right = 1.0;
     }
   }
-  ROS_INFO("Thrust(FINAL): [LEFT]%.3lf, [RIGHT]%.3lf\n", T_Left, T_Right);
+  ROS_INFO("Thrust(Norm): [LEFT]%.3lf, [RIGHT]%.3lf", T_Left, T_Right);
   
+
+  /* -5- Limit Thrust change */
+  //Left
+  if( T_Left - thrust_left_old > max_thrust_deviation;)
+  {
+	T_Left = thrust_left_old + max_thrust_deviation;
+  }
+  else
+  {
+	T_Left = thrust_left_old + T_Left;
+  }
+
+  //Right
+  if( T_Right - thrust_right_old > max_thrust_deviation;)
+  {
+	T_Right = thrust_right_old + max_thrust_deviation;
+  }
+  else
+  {
+	T_Right = thrust_right_old + T_Right;
+  }
+  ROS_INFO("Thrust(FINAL): [LEFT]%.3lf, [RIGHT]%.3lf\n", T_Left, T_Right);
+
+
+  /* -6- Send Thruster Topic */
+  mtx_lock();
   thrust_cmd_.command.left_thrust_cmd = T_Left;
   thrust_cmd_.command.right_thrust_cmd = T_Right;
   mtx_.unlock();
+
+
+  /* -7- Refresh Old Values */
+  velocity_difference_y_old = velocity_difference_y; 
+  velocity_difference_yaw_old = velocity_difference_yaw;
+  thrust_left_old_ = T_Left;
+  thrust_right_old_ = T_Right;
   
 }
 
